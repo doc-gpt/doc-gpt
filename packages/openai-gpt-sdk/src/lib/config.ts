4cc7e1a7e8d99661e2709f8e20fb4d6c8e772130
doc-code <bellini.francescob@gmail.com>
2023-03-03 03:40:55 +0100

    Add config file with OpenAI /v1/chat/completions api url

---
+++ packages/openai-gpt-sdk/src/lib/config.ts
@@ -0,0 +1,6 @@
+/**
+ * URL for the OpenAI `/v1/chat/completions` api.
+ * https://platform.openai.com/docs/api-reference/chat
+ */
+export const OpenAIChatCompletionApi =
+  "https://api.openai.com/v1/chat/completions";
