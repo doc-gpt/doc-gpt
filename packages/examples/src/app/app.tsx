1052663b6d560d44f0a21be912b94eddc9603e66
doc-code <bellini.francescob@gmail.com>
2023-03-03 03:43:53 +0100

    Create basic example page with apiKey input, system message and prompt textarea, and a button to execute.

--- packages/examples/src/app/app.tsx
+++ packages/examples/src/app/app.tsx
@@ -1,55 +1,75 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
-
-import { Route, Routes, Link } from 'react-router-dom';
+import { useCallback, useState } from 'react';
+import GptSDK from 'packages/openai-gpt-sdk/src/lib/openai-gpt-sdk';
 
 export function App() {
-  return (
-    <>
-      <NxWelcome title="examples" />
+	const [message, setMessage] = useState('Waiting for you');
+	const [system, setSystem] = useState('');
+	const [apiKey, setApiKey] = useState('');
+	const [prompt, setPrompt] = useState('');
 
-      <div />
+	const generate = useCallback(() => {
+		const sdk = new GptSDK({
+			apiKey: apiKey,
+			defaultSystemMessage: system,
+		});
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
-    </>
-  );
+		sdk
+			.Chat([
+				{
+					role: 'user',
+					content: prompt,
+				},
+			])
+			.then((res) => setMessage(res.choices[0].message.content));
+	}, [apiKey, prompt, system, setMessage]);
+
+	return (
+		<>
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					padding: '100px 20vw',
+					color: 'white',
+				}}
+			>
+				<label>ApiKey</label>
+				<input
+					type="password"
+          placeholder='Set your OpenAI Api Key (required)'
+					onChange={(ev) => setApiKey(ev.target.value)}
+					value={apiKey}
+          style={{outlineColor: 'red'}}
+				/>
+
+				<label>System</label>
+				<textarea
+          placeholder='Set an optional system message to set the behaviour'
+					onChange={(ev) => setSystem(ev.target.value)}
+					value={system}
+          style={{outlineColor: 'orange'}}
+				/>
+
+				<label>Propmt</label>
+				<textarea
+          placeholder='Write your prompt and click "Get a response"'
+					onChange={(ev) => setPrompt(ev.target.value)}
+					value={prompt}
+          style={{outline: 'dashed 1px #9f9f9f'}}
+				/>
+
+				<button style={{ margin: '50px 100px' }} onClick={generate}>
+					Get a response
+				</button>
+			</div>
+
+			<p style={{ color: 'white', textAlign: 'center', padding: '0 20vw', marginBottom: '35vh' }}>
+				{message}
+			</p>
+		</>
+	);
 }
 
 export default App;
